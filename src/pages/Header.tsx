758d7b9ecb4c74d4cf9ff8cef6aa1bc550a01d21
abdulloh <abdullohyusupov71@gmail.com>
2026-09-25 13:53:06 +0300

    p tegi qoshildi

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 function Header() {
   return (
@@ -38,6 +38,7 @@ function Header() {
         </select>
       </div>
       <button>tasdiqlash</button>
+      <p>Lorem ipsum dolor sit amet.</p>
     </header>
   )
 }
